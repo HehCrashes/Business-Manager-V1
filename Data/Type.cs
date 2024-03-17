bce85fe4793f44ba89d0a8c39eeac5174e668bb0
HehCrashes <3095217291@qq.com>
2024-03-17 22:26:27 +0800

    添加项目文件。

---
+++ Data/Type.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVE.Data
+{
+    public class Type
+    {
+        float capacity { get; set; } = -1f;
+        string description { get; set; } = string.Empty;
+        int group_id { get; set; } = -1;
+        float mass { get; set; } = -1f;
+        string name { get; set; } = string.Empty;
+        float packaged_volume { get; set; } = -1f;
+        int portion_size { get; set; } = -1;
+        bool published { get; set; } = true;
+        float radius { get; set; } = -1f;
+        int id { get; set; } = -1;
+        float volume { get; set; } = -1f;
+        public Type(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public Type(float capacity, string description, int group_id, float mass, string name, float packaged_volume, int portion_size, bool published, float radius, int id, float volume)
+        {
+            this.capacity = capacity;
+            this.description = description;
+            this.group_id = group_id;
+            this.mass = mass;
+            this.name = name;
+            this.packaged_volume = packaged_volume;
+            this.portion_size = portion_size;
+            this.published = published;
+            this.radius = radius;
+            this.id = id;
+            this.volume = volume;
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EVE.Utils
+{
+    public static class Utils
+    {
+
+        public static int[] MergeArray(int[] a, int[] b)
+        {
+            int[] temp = new int[a.Length + b.Length];
+            a.CopyTo(temp, 0);
+            b.CopyTo(temp, a.Length);
+            return temp;
+        }
+        public static int[] ResolveIDList(string s)
+        {
+            string a = s.Replace("[", "");
+            string b = a.Replace("]", "");
+            string[] sArray = b.Split(',');
+            int[] ret = Array.ConvertAll(sArray, value => Convert.ToInt32(value));
+            return ret;
+        }
+        public static string GetHTMLByURL(string URL)
+        {
+            try
+            {
+                string pageHtml = "";
+                WebClient MyWebClient = new WebClient();
+                MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+                Byte[] pageData = MyWebClient.DownloadData(URL); //从指定网站下载数据
+                MemoryStream ms = new MemoryStream(pageData);
+                using (StreamReader sr = new StreamReader(ms, Encoding.GetEncoding("UTF-8")))
+                {
+                    pageHtml = sr.ReadLine();
+                }
+                return Regex.Unescape(pageHtml);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+    }
+}
